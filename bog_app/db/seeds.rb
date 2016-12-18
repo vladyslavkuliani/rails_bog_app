dc33cf7680f0553a45f80649fdf71af4afc0cfdc
Vladyslav Kuliani <vladyslavkuliani@Vladyslavs-MacBook-Pro.local>
2016-12-17 21:51:31 -0800

    created creatures controller and model

--- bog_app/db/seeds.rb
+++ bog_app/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Creature.delete_all
+
+Creature.create({name:"Yoda", description:"Little green man"})
+Creature.create({name: "Luke", description: "Jedi"})
+Creature.create({name: "Darth Vader", description: "Father of Luke"})
