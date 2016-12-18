9ce5fa1fd33116b8108090f8ddbd3461e5c3ba6a
Vladyslav Kuliani <vladyslavkuliani@Vladyslavs-MacBook-Pro.local>
2016-12-17 23:20:52 -0800

    destroy creature/finished CRUD

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -30,6 +30,11 @@ class CreaturesController < ApplicationController
     redirect_to creature_path(creature)
   end
 
+  def destroy
+    Creature.find_by(id: params[:id]).destroy
+    redirect_to creatures_path
+  end
+
   private
   def creature_params
     params.require(:creature).permit(:name, :description)
