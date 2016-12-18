76b3986b556fc1b2e77ec06c0c5cb67e3fc333e2
Vladyslav Kuliani <vladyslavkuliani@Vladyslavs-MacBook-Pro.local>
2016-12-17 22:58:11 -0800

    edit/update

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -20,6 +20,16 @@ class CreaturesController < ApplicationController
     @creature = Creature.find_by(id: params[:id])
   end
 
+  def edit
+    @creature = Creature.find_by(id: params[:id])
+  end
+
+  def update
+    creature = Creature.find_by(id: params[:id])
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
   private
   def creature_params
     params.require(:creature).permit(:name, :description)
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get "/creatures/new", to: "creatures#new", as: "new_creature"
   post "/creatures", to: "creatures#create"
   get "/creature/:id", to: "creatures#show", as: "creature"
+  get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
+  patch "/creature/:id", to: "creatures#update"
 end
