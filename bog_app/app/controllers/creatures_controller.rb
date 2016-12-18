f14cedfa1242ac4fd921cdda38deee185b475526
Vladyslav Kuliani <vladyslavkuliani@Vladyslavs-MacBook-Pro.local>
2016-12-17 22:12:59 -0800

    added new, create, show for creatures

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -2,4 +2,26 @@ class CreaturesController < ApplicationController
   def index
     @creatures = Creature.all
   end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    creature = Creature.new(creature_params)
+    if creature.save
+      redirect_to creature_path(creature)
+    else
+      redirect_to new_creature_path
+    end
+  end
+
+  def show
+    @creature = Creature.find_by(id: params[:id])
+  end
+
+  private
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
 end
