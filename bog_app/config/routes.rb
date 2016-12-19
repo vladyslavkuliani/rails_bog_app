d6a451f0bae580b66519a710b9a91f45e3bd3410
Vladyslav Kuliani <vladyslavkuliani@Vladyslavs-MacBook-Pro.local>
2016-12-18 21:24:20 -0800

    new/create creature

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "creatures#index"
+
+  get "/creatures", to: "creatures#index", as: "creatures"
+  get "/creatures/new", to: "creatures#new", as: "new_creature"
+  post "/creatures", to: "creatures#create"
 end
